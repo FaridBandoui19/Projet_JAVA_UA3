1de10c6887825db378510c24f39d9fc30dc313a6
FaridBandoui19 <farid.bandoui@gmail.com>
2025-08-13 05:48:27 -0400

    modification: ajout de commentaires

--- CsvFileWriter.java
+++ CsvFileWriter.java
@@ -2,13 +2,17 @@ import java.io.*;
 import java.util.*;
 
 public class CsvFileWriter implements ICsvWriter {
-    @Override
+    @Override // indique qu’on implémente la méthode de l’interface.
     public void write(String path, List<String[]> data) {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-            for (String[] row : data) {
-                bw.write(String.join(",", row));
-                bw.newLine();
+
+        // try-with-resources : ferme automatiquement le fichier à la fin ou en cas
+        // d’erreur.
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) { // Ouverture du fichier en écriture
+            for (String[] row : data) { // Parcours des lignes de données
+                bw.write(String.join(",", row)); // joint les valeurs avec des virgules
+                bw.newLine(); // ajoute une nouvelle ligne
             }
+            // Gestion des erreurs
         } catch (IOException e) {
             System.err.println("Erreur d'écriture du fichier : " + e.getMessage());
         }
